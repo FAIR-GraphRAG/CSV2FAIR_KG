1cd13588403497c635498bf482253fb96b0a97b6
MarlenaFlueh <marlena.flueh@rwth-aachen.de>
2025-04-06 15:50:27 +0200

    update config with umls

--- config/config.py
+++ config/config.py
@@ -4,8 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DEPLOYMENT = os.getenv("DEPLOYMENT")
+
 OLLAMA_URL = os.getenv("OLLAMA_URL")
+
 AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
 AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
 AZURE_API_VERSION = os.getenv("AZURE_API_VERSION")
+
 NUM_TEST_DOCUMENTS = int(os.getenv("NUM_TEST_DOCUMENTS"))
+
+UMLS_API_KEY = os.getenv("UMLS_API_KEY")
+UMLS_USERNAME = os.getenv("UMLS_USERNAME")
+UMLS_LICENSE_CODE = os.getenv("UMLS_LICENSE_CODE")
