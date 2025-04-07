f7f9889704b4824f80d84c6fac2ba01c8bb4462f
MarlenaFlueh <marlena.flueh@rwth-aachen.de>
2025-04-07 17:36:07 +0200

    update

--- config/config.py
+++ config/config.py
@@ -3,16 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DEPLOYMENT = os.getenv("DEPLOYMENT")
-
 OLLAMA_URL = os.getenv("OLLAMA_URL")
 
 AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
 AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
 AZURE_API_VERSION = os.getenv("AZURE_API_VERSION")
 
-NUM_TEST_DOCUMENTS = int(os.getenv("NUM_TEST_DOCUMENTS"))
-
 UMLS_API_KEY = os.getenv("UMLS_API_KEY")
 UMLS_USERNAME = os.getenv("UMLS_USERNAME")
 UMLS_LICENSE_CODE = os.getenv("UMLS_LICENSE_CODE")
+
+# Threshold for cosine similarity (relation extraction)
+SIMILARITY_THRESHOLD_INNER = 0.5
+SIMILARITY_THRESHOLD_CROSS = 0.2
+
+# Num of test documents schema
+NUM_TEST_DOCUMENTS = 3
+
+# available models OpenAI: gpt-4o-mini, gpt-4
+# available models open source: llama3.2:3b, deepseek-r1:7b, deepseek-r1:70b, llama3.3:70b
+DEPLOYMENT = "gpt-4o-mini"
