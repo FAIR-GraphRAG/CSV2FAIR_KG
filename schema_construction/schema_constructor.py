bc0e3a3a24b1b177eeb73c9a4cbce1f936e1cd84
MarlenaFlueh <marlena.flueh@rwth-aachen.de>
2025-04-07 17:37:27 +0200

    clean up

--- schema_construction/schema_constructor.py
+++ schema_construction/schema_constructor.py
@@ -6,36 +6,8 @@ Based on the header of the .csv files, the schema_constructor will generate a sc
 - The schema will be saved in schema/schema.json.
 """
 
-from langchain_community.document_loaders.csv_loader import CSVLoader
-import os
 from language_model.chat_client_factory import client_selector
-from utils.helper import save_json
-
-
-def load_docs(report_folder):
-    documents = []
-    for filename in os.listdir(report_folder):
-        if filename.endswith(".csv"):
-            file_path = os.path.join(report_folder, filename)
-            loader = CSVLoader(file_path=file_path)
-            data = loader.load()
-            if data:  # Make sure there's at least one row
-                documents.append(data[0])  # Append the header/first row
-    return documents
-
-
-def find_docs_in_folder(csv_dir, data_dir):
-    """
-    For each csv_dir in data_dir call load_doc function
-    return list of Documents: [Document, Document]
-    """
-    documents = []
-    for current_root, dirs, files in os.walk(data_dir):
-        for dir_name in dirs:
-            if dir_name == csv_dir:
-                csv_data_path = os.path.join(current_root, dir_name)
-                documents.extend(load_docs(csv_data_path))
-    return documents
+from utils.helper import save_json, find_docs_in_folder
 
 
 def create_schema():
--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,6 @@
 import json
+from langchain_community.document_loaders.csv_loader import CSVLoader
+import os
 
 
 def read_json(file_path):
@@ -11,3 +13,33 @@ def save_json(file_path, json_data):
     with open(file_path, "w", encoding="utf-8") as json_file:
         json.dump(json_data, json_file, indent=4, ensure_ascii=False)
     print(f"\nJSON saved to {file_path}")
+
+
+def load_docs(report_folder, cut_file=True):
+    documents = []
+    for filename in os.listdir(report_folder):
+
+        if filename.endswith(".csv"):
+            file_path = os.path.join(report_folder, filename)
+            loader = CSVLoader(file_path=file_path)
+            data = loader.load()
+            if data:  # Make sure there's at least one row
+                if cut_file:
+                    documents.append(data[0])  # Append the header + first row
+                else:
+                    documents.append(data)  # Append all rows
+    return documents
+
+
+def find_docs_in_folder(csv_dir, data_dir, cut_file=True):
+    """
+    For each csv_dir in data_dir call load_doc function
+    return list of Documents: [Document, Document]
+    """
+    documents = []
+    for current_root, dirs, files in os.walk(data_dir):
+        for dir_name in dirs:
+            if dir_name == csv_dir:
+                csv_data_path = os.path.join(current_root, dir_name)
+                documents.extend(load_docs(csv_data_path, cut_file=cut_file))
+    return documents
