6e94aafb961a1f8816543893fb5ed170896995c5
MarlenaFlueh <marlena.flueh@rwth-aachen.de>
2025-05-02 13:55:07 +0200

    add new endpoints

--- FAIRification/fair_api/main.py
+++ FAIRification/fair_api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from FAIRification.fair_api.views import dataset_landing
+from FAIRification.fair_api.views import dataset_landing, research_obj_landing
 
 app = FastAPI()
 
@@ -12,3 +12,8 @@ async def dataset_page():
 @app.get("/ds/{dataset_id}")
 async def dataset_page(dataset_id: str, request: Request):
     return await dataset_landing(dataset_id, request)
+
+
+@app.get("/ds/{dataset_id}/{research_obj_id}")
+async def research_obj_page(dataset_id: str, research_obj_id: str, request: Request):
+    return await research_obj_landing(dataset_id, research_obj_id, request)
--- FAIRification/fair_api/views.py
+++ FAIRification/fair_api/views.py
@@ -5,8 +5,26 @@ from utils.helper import read_json
 
 templates = Jinja2Templates(directory="templates")
 path_metadata = "data/extracted_data/metadata/"
+path_research_obj = "data/extracted_data/filled_schema/{dataset_id}"
 
 
-async def dataset_landing(dataset_id: str, request: Request):
+async def dataset_landing(dataset_id: str):
     data = read_json(path_metadata + dataset_id)
     return JSONResponse(content=data, media_type="application/json")
+
+
+async def research_obj_landing(dataset_id: str, research_obj_id: str):
+    metadata = read_json(path_metadata + dataset_id)
+    data = read_json(path_research_obj.format(dataset_id=dataset_id))
+    # Check if research_obj_id exists in data
+    item = [
+        v
+        for v in data["level_1"]["properties"].values()
+        if v.get("pid") == research_obj_id
+    ]
+    modified_item = item[0]
+    modified_item["properties"] = ""
+    obj_metadata = {**modified_item, "dataset": metadata}
+
+    if item:
+        return JSONResponse(content=obj_metadata, media_type="application/json")
