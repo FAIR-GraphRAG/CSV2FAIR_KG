177c08ce66bdc3455c3b1fe9cb56eb429737c848
Marlena Flüh <m.flueh@tu-braunschweig.de>
2025-06-12 11:12:15 +0200

    update config to use opensource model

--- config/config.py
+++ config/config.py
@@ -9,6 +9,10 @@ AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
 AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
 AZURE_API_VERSION = os.getenv("AZURE_API_VERSION")
 
+AZURE_OPEN_SOURCE_KEY = os.getenv("AZURE_OPEN_SOURCE_KEY")
+AZURE_OPEN_SOURCE_ENDPOINT = os.getenv("AZURE_OPEN_SOURCE_ENDPOINT")
+AZURE_OPEN_SOURCE_DEPLOYMENT_NAME = os.getenv("OPEN_SOURCE_DEPLOYMENT_NAME")
+
 UMLS_API_KEY = os.getenv("UMLS_API_KEY")
 UMLS_USERNAME = os.getenv("UMLS_USERNAME")
 UMLS_LICENSE_CODE = os.getenv("UMLS_LICENSE_CODE")
@@ -22,4 +26,15 @@ NUM_TEST_DOCUMENTS = 3
 
 # available models OpenAI: gpt-4o-mini, gpt-4
 # available models open source: llama3.2:3b, deepseek-r1:7b, deepseek-r1:70b, llama3.3:70b
-DEPLOYMENT = "gpt-4o-mini"
+# gpt-4o-mini, gpt-4, Llama-3.3-70B-Instruct
+DEPLOYMENT_NAME = "gpt-4o-mini"
+
+BIOONTOLOGY_API_KEY = os.getenv("API_KEY_BIOONTOLOGY")
+BIOONTOLOGY_API_URL = "https://data.bioontology.org"
+
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_INSTANCE = os.getenv("NEO4J_INSTANCE")
+NEO4J_URL = os.getenv("NEO4J_URL")
+
+ENTITY_CLASS_LIST = ["pathway", "GO-BP"]
