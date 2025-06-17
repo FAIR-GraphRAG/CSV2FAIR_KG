65b5889c1720ef8d0641ca910c9fdfaeded2f40b
Marlena Flüh <m.flueh@tu-braunschweig.de>
2025-06-17 11:51:26 +0200

    improve

--- config/config.py
+++ config/config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OLLAMA_URL = os.getenv("OLLAMA_URL")
-
 AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
 AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
 AZURE_API_VERSION = os.getenv("AZURE_API_VERSION")
@@ -13,28 +11,24 @@ AZURE_OPEN_SOURCE_KEY = os.getenv("AZURE_OPEN_SOURCE_KEY")
 AZURE_OPEN_SOURCE_ENDPOINT = os.getenv("AZURE_OPEN_SOURCE_ENDPOINT")
 AZURE_OPEN_SOURCE_DEPLOYMENT_NAME = os.getenv("OPEN_SOURCE_DEPLOYMENT_NAME")
 
-UMLS_API_KEY = os.getenv("UMLS_API_KEY")
-UMLS_USERNAME = os.getenv("UMLS_USERNAME")
-UMLS_LICENSE_CODE = os.getenv("UMLS_LICENSE_CODE")
-
-# Threshold for cosine similarity (relation extraction)
-SIMILARITY_THRESHOLD_INNER = 0.5
-SIMILARITY_THRESHOLD_CROSS = 0.2
-
-# Num of test documents schema
-NUM_TEST_DOCUMENTS = 3
-
-# available models OpenAI: gpt-4o-mini, gpt-4
-# available models open source: llama3.2:3b, deepseek-r1:7b, deepseek-r1:70b, llama3.3:70b
-# gpt-4o-mini, gpt-4, Llama-3.3-70B-Instruct
-DEPLOYMENT_NAME = "gpt-4o-mini"
+# Models: gpt-4o-mini, Llama-3.3-70B-Instruct
+DEPLOYMENT_NAME = "Llama-3.3-70B-Instruct"
 
 BIOONTOLOGY_API_KEY = os.getenv("API_KEY_BIOONTOLOGY")
 BIOONTOLOGY_API_URL = "https://data.bioontology.org"
 
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 NEO4J_INSTANCE = os.getenv("NEO4J_INSTANCE")
-NEO4J_URL = os.getenv("NEO4J_URL")
 
+# FAIR=True or Non-FAIR=False
+FAIR_GRAPH = True
+
+if FAIR_GRAPH:
+    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD_FAIR")
+    NEO4J_URL = os.getenv("NEO4J_URL_FAIR")
+else:
+    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD_NON-FAIR")
+    NEO4J_URL = os.getenv("NEO4J_URL_NON-FAIR")
+
+# User-defined entities
 ENTITY_CLASS_LIST = ["pathway", "GO-BP"]
